401ffb4138f0a3b8bb218b51c8195460ad633448
Jarod Lam <10664255+jarodlam@users.noreply.github.com>
2025-12-03 17:31:11 +1100

    day 1 part 1

--- src/day01.rs
+++ src/day01.rs
@@ -5,12 +5,53 @@ pub struct Day01 {}
 impl Solution for Day01 {
     type Output = u32;
 
-    fn part1(_input: &str) -> Self::Output {
-        0
+    fn part1(input: &str) -> Self::Output {
+        let mut out: u32 = 0;
+        let mut pos: i32 = 50;
+        for line in input.lines() {
+            let (dir_str, dist_str) = line.split_at(1);
+            let dir: i32 = if dir_str == "L" { -1 } else { 1 };
+            let dist: u32 = dist_str.parse().unwrap();
+
+            pos += dir * (dist as i32);
+            while pos < 0 {
+                pos += 100;
+            }
+            while pos > 99 {
+                pos -= 100;
+            }
+
+            if pos == 0 {
+                out += 1;
+            }
+        }
+        out
     }
 
-    fn part2(_input: &str) -> Self::Output {
-        0
+    fn part2(input: &str) -> Self::Output {
+        let mut out: u32 = 0;
+        let mut pos: i32 = 50;
+        for line in input.lines() {
+            let (dir_str, dist_str) = line.split_at(1);
+            let dir: i32 = if dir_str == "L" { -1 } else { 1 };
+            let dist: u32 = dist_str.parse().unwrap();
+
+            pos += dir * (dist as i32);
+            while pos < 0 {
+                pos += 100;
+                out += 1;
+            }
+            while pos > 99 {
+                pos -= 100;
+                out += 1;
+            }
+            println!("{}: {} {}", line, pos, out);
+        }
+        // Edge case if we finish on 0
+        if pos == 0 {
+            out += 1;
+        }
+        out
     }
 }
 
@@ -35,5 +76,7 @@ L82";
     }
 
     #[test]
-    fn test_part2() {}
+    fn test_part2() {
+        assert_eq!(Day01::part2(TEST_INPUT), 6);
+    }
 }
